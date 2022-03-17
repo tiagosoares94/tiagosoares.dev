5ec9c18bfaaf84b037b776c67890af1d261b644c
tiagosoares94 <tsoares.sjc@gmail.com>
2022-03-17 13:42:36 -0300

    Altera página de contato

--- wp-content/themes/theme/page-contato.php
+++ wp-content/themes/theme/page-contato.php
@@ -1,9 +1,12 @@
 <?php include("partials/header.php"); ?>
 
 <section class="container">
-    <h1 class="mt-5">Entre em contato</h1><br>
-    <p>Plugin em desenvolvimento</p><br>
-    <?= the_content(); ?>
+    <div class="container-wrapper text-center mt-5 mb-5">
+        <h1 class="mt-5">Entre em contato</h1><br>
+        <p class="text-danger">Plugin em desenvolvimento - Entre em contato pelo email: <br><a href="mailto:tsoares.sjc@gmail.com">tsoares.sjc@gmail.com</a></p><br>
+        <?= the_content(); ?>
+    </div>
+
 </section>
 
 
